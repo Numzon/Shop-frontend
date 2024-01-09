36a3126e6935ca95feb01ad79c8c3490249cc4e2
Daniel Mlynarczyk <Daniel.Mlynarczyk@cshark.com>
2024-01-09 16:13:29 +0100

    use

--- apps/shop/src/api/hooks/SpecificationPattern/useAddSpecificationPattern.ts
+++ apps/shop/src/api/hooks/SpecificationPattern/useAddSpecificationPattern.ts
@@ -5,7 +5,6 @@ import { addSpecificationPattern } from "../../requests/specificationPattern";
 export const useAddSpecificationPattern = () =>
   useMutation({
     mutationFn: async (model: CreateSpecificationPatternRequest) => {
-      console.log(model);
       const { data } = await addSpecificationPattern(model);
       return data;
     },
